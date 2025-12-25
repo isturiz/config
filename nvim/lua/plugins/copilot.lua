9dd9516dda4da3e5c8430eacf1550bd59c2bc8be
Mauricio Istúriz <isturizmauricio@gmail.com>
2025-12-25 15:48:25 -0400

    nvim(copilot.lua): disable telemetry

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,25 +2,29 @@ return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   event = "InsertEnter",
-  config = function()
-    require("copilot").setup({})
-  end,
 
-  workspace_folders = {
-    "~/workspace",
-    "~/config",
+  opts = {
+    server_opts_overrides = {
+      settings = {
+        telemetry = {
+          telemetryLevel = "off",
+        },
+      },
+    },
+
+    workspace_folders = {
+      "~/workspace",
+      "~/config",
+    },
+
+    should_attach = function(_, bufname)
+      if string.match(bufname, "env") then
+        return false
+      end
+      return true
+    end,
+
+    suggestion = { enabled = false },
+    panel = { enabled = false },
   },
-
-  should_attach = function(_, bufname)
-    if string.match(bufname, "env") then
-      return false
-    end
-
-    return true
-  end,
-
-  suggestion = { enabled = false },
-  panel = { enabled = false },
-
 }
-
