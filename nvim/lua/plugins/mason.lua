839ee8307aebfe16d0e5140b292ad27ace6c3854
Mauricio Istúriz <isturizmauricio@gmail.com>
2026-01-02 09:09:30 -0400

    Mason: add keybinding to open Mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,4 +1,11 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+  "mason-org/mason.nvim",
+  opts = {},
+  keys = {
+    {
+      "<leader>M",
+      ':Mason<CR>',
+      desc = "Open Mason",
+    },
+  },
 }
